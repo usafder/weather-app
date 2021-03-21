995cbef96a5999d483c684c39f40bf8a7ab9d717
usafder <36886437+usafder@users.noreply.github.com>
2021-03-22 04:02:39 +0500

    Added weather API

---
+++ src/api/weather.ts
@@ -0,0 +1,26 @@
+import { apiClient, apiKey } from './api-client';
+
+/** Will fetch the weather data based on the city name. */
+export const getWeatherDataUsingCityName = async (city: string) => {
+  try {
+    const queryParams = `?q=${city}&appId=${apiKey}&units=metric`;
+    const response = await apiClient.get(queryParams);
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
+
+/** Will fetch the weather data based on the latitude and longitude. */
+export const getWeatherDataUsingLatAndLong = async (
+  latitude: number,
+  longitude: number
+) => {
+  try {
+    const queryParams = `?lat=${latitude}&lon=${longitude}&appId=${apiKey}&units=metric`;
+    const response = await apiClient.get(queryParams);
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
