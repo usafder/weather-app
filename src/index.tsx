875b5acb1e5a3363d1e31d440e3bfd578f63d9ae
usafder <36886437+usafder@users.noreply.github.com>
2021-03-23 01:53:22 +0500

    Removed web vitals

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import store from './state/store';
 
 ReactDOM.render(
@@ -16,8 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
