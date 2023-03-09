7a8a8d87d837b6ad3228f8a742ed1ceec35299f0
Usman <36886437+usafder@users.noreply.github.com>
2023-03-09 18:09:24 +0500

    REFACTOR: shifted appCss definition outside component

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,20 @@ import { css } from '@emotion/react';
 import Router from './router';
 import 'weathericons/css/weather-icons.min.css';
 
+const appCss = css({
+  textAlign: 'center',
+  position: 'fixed',
+  padding: 0,
+  margin: 0,
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+});
+
 function App() {
   return (
-    <div
-      css={css`
-        text-align: center;
-        position: fixed;
-        padding: 0;
-        margin: 0;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-      `}
-    >
+    <div css={appCss}>
       <Router />
     </div>
   );
